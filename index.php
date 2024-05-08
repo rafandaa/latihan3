e1afebdd20401b341d35d2dafab38828cf5dbfaf
rafandaa <trdvaan12@gmail.com>
2024-05-08 11:27:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,0">
+        <title>kalkulator</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+    <h1>kalkulator</h1>
+        <form action="hasil.php">
+            <input type="number" name="angka1">
+            <select name="operator">
+                <option>+</option>
+                <option>-</option>
+                <option>:</option>
+                <option>x</option>
+            </select>
+            <input type="number" name="angka2">
+            <button type="submit">hitung</button>
+        </form>
+    </body>
+</html>
